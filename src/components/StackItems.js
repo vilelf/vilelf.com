ecdfe4edd8a6c9a7aa58b9a8502c3c557c46392e
vilelf <vilelf.dev@gmail.com>
2024-03-03 20:54:07 -0300

    chore: update stack

--- src/components/StackItems.js
+++ src/components/StackItems.js
@@ -1,136 +1,126 @@
-import actionsIcon from "assets/img/actions.png"
-import ansibleIcon from "assets/img/ansible.png"
-import apigatewayIcon from "assets/img/aws-api-gateway.png"
-import awsIcon from "assets/img/aws.jpeg"
-import circleciIcon from "assets/img/circleci.jpg"
+import actionsIcon from "assets/img/actions.png";
+import ansibleIcon from "assets/img/ansible.png";
+import awsIcon from "assets/img/aws.jpeg";
+import circleciIcon from "assets/img/circleci.jpg";
 import djangoIcon from "assets/img/django.png";
 import dockerIcon from "assets/img/docker.png";
-import elasticsearchIcon from "assets/img/elasticsearch.png"
-import githubIcon from "assets/img/github.png"
-import gitIcon from "assets/img/git.png"
-import helmIcon from "assets/img/helm.jpeg"
-import istioIcon from "assets/img/istio.jpeg"
-import kubernetesIcon from "assets/img/kubernetes.png"
-import lambdaIcon from "assets/img/aws-lambda.png"
-import linuxIcon from "assets/img/linux.png"
+import elasticsearchIcon from "assets/img/elasticsearch.png";
+import githubIcon from "assets/img/github.png";
+import gitIcon from "assets/img/git.png";
+import gitlabIcon from "assets/img/gitlab.jpg";
+import helmIcon from "assets/img/helm.jpeg";
+import istioIcon from "assets/img/istio.jpeg";
+import kubernetesIcon from "assets/img/kubernetes.png";
+import linuxIcon from "assets/img/linux.png";
 import nginxIcon from "assets/img/nginx.png";
 import postgresIcon from "assets/img/postgres.png";
-import prometheusIcon from "assets/img/prometheus.png"
+import prometheusIcon from "assets/img/prometheus.png";
 import pythonIcon from "assets/img/python.png";
-import s3Icon from "assets/img/amazon-s3.png";
 import sentryIcon from "assets/img/sentry.png";
 import serverlessIcon from "assets/img/serverless.png";
-import terraformIcon from "assets/img/terraform.png"
-import vscIcon from "assets/img/vsc.png"
-
+import terraformIcon from "assets/img/terraform.png";
+import vscIcon from "assets/img/vsc.png";
 
 export const aws = {
-    icon: awsIcon,
-    name: 'AWS'
-}
+  icon: awsIcon,
+  name: "AWS",
+};
 
 export const python = {
-    icon: pythonIcon,
-    name: "Python"
-}
+  icon: pythonIcon,
+  name: "Python",
+};
 
 export const django = {
-    icon: djangoIcon,
-    name: 'Django'
-}
+  icon: djangoIcon,
+  name: "Django",
+};
 
 export const postgres = {
-    icon: postgresIcon,
-    name: 'PostgreSQL'
-}
+  icon: postgresIcon,
+  name: "PostgreSQL",
+};
 
 export const nginx = {
-    icon: nginxIcon,
-    name: 'NGINX'
-}
+  icon: nginxIcon,
+  name: "NGINX",
+};
 
 export const docker = {
-    icon: dockerIcon,
-    name: 'Docker'
-}
+  icon: dockerIcon,
+  name: "Docker",
+};
 
 export const serverless = {
-    icon: serverlessIcon,
-    name: 'Serverless'
-}
+  icon: serverlessIcon,
+  name: "Serverless",
+};
 
 export const git = {
-    icon: gitIcon,
-    name: 'Git'
-}
+  icon: gitIcon,
+  name: "Git",
+};
 export const github = {
-    icon: githubIcon,
-    name: 'Github'
-}
+  icon: githubIcon,
+  name: "Github",
+};
 export const vscode = {
-    icon: vscIcon,
-    name: 'VS Code'
-}
+  icon: vscIcon,
+  name: "VS Code",
+};
 export const circleci = {
-    icon: circleciIcon,
-    name: 'Circle CI'
-}
-export const s3 = {
-    icon: s3Icon,
-    name: 'AWS S3'
-}
-export const lambda = {
-    icon: lambdaIcon,
-    name: 'AWS Lambda'
-}
-export const apiGateway = {
-    icon: apigatewayIcon,
-    name: 'AWS ApiGateway'
-}
+  icon: circleciIcon,
+  name: "Circle CI",
+};
 export const elasticsearch = {
-    icon: elasticsearchIcon,
-    name: 'Elasticsearch'
-}
+  icon: elasticsearchIcon,
+  name: "Elasticsearch",
+};
 export const githubActions = {
-    icon: actionsIcon,
-    name: 'Github Actions'
-}
-export const terraform =  {
-    icon: terraformIcon,
-    name: 'Terraform'
-}
+  icon: actionsIcon,
+  name: "Github Actions",
+};
+export const terraform = {
+  icon: terraformIcon,
+  name: "Terraform",
+};
 
-export const ansible =  {
-    icon: ansibleIcon,
-    name: 'Ansible'
-}
+export const ansible = {
+  icon: ansibleIcon,
+  name: "Ansible",
+};
 
-export const linux =  {
-    icon: linuxIcon,
-    name: 'Linux'
-}
+export const linux = {
+  icon: linuxIcon,
+  name: "Linux",
+};
 
 export const kubernetes = {
-    icon: kubernetesIcon,
-    name: 'Kubernetes'
-}
+  icon: kubernetesIcon,
+  name: "Kubernetes",
+};
 
 export const helm = {
-    icon: helmIcon,
-    name: 'Helm'
-}
+  icon: helmIcon,
+  name: "Helm",
+};
 
 export const istio = {
-    icon: istioIcon,
-    name: 'Istio'
-}
+  icon: istioIcon,
+  name: "Istio",
+};
 
 export const prometheus = {
-    icon: prometheusIcon,
-    name: 'Prometheus'
-}
+  icon: prometheusIcon,
+  name: "Prometheus",
+};
 
 export const sentry = {
-    icon: sentryIcon,
-    name: 'Sentry'
-}
+  icon: sentryIcon,
+  name: "Sentry",
+};
+
+export const gitlab = {
+  icon: gitlabIcon,
+  name: "Gitlab CI",
+};
